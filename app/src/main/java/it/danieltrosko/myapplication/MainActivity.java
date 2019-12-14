012f0204582c0f896c7617f1e3c8b4bf48bceba7
Daniel <daniel.trosko@gmail.com>
2019-12-14 14:52:36 +0100

    first

---
+++ app/src/main/java/it/danieltrosko/myapplication/MainActivity.java
@@ -0,0 +1,130 @@
+package it.danieltrosko.myapplication;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.JsonReader;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        final Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.textView);
+
+
+button.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                URL url = null;
+                try {
+                    url = new URL("http://10.0.2.2:8080/getwaitressbyid?id=1");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                HttpURLConnection myConnection = null;
+                try {
+                    myConnection = (HttpURLConnection) url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (myConnection.getResponseCode()== 200){
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader reader = new InputStreamReader(responseBody, "UTF-8");
+                        JsonReader jsonReader = new JsonReader(reader);
+
+                        jsonReader.beginObject();
+                        String username = null;
+
+                        while (jsonReader.hasNext()){
+                            String json = jsonReader.nextName();
+
+                            if (json.equals("username")){
+                                username = jsonReader.nextString();
+                            }else {
+                                jsonReader.skipValue();
+                            }
+                        }
+                        textView.setText(username);
+                        jsonReader.close();
+                        myConnection.disconnect();
+
+
+
+
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+
+
+    }
+});
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
